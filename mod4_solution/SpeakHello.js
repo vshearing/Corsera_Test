d0ad5d71ffa5819ce3188becd31c2376c54e9267
vshearing <68555250+vshearing@users.noreply.github.com>
2022-08-12 21:34:44 -0500

    Add files via upload

--- mod4_solution/SpeakHello.js
+++ mod4_solution/SpeakHello.js
@@ -9,8 +9,4 @@ helloSpeaker.speak = function (names) {
 window.helloSpeaker = helloSpeaker;
 })(window);
 
-// STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
-// 'helloSpeaker' on the global scope as well.
-// See Lecture 52, part 2
-// (Note, Step 6 will be done in the SpeakGoodBye.js file.)
-// xxxx.xxxx = helloSpeaker;
+
